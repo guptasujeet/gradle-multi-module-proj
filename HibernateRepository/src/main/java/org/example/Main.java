5538a86389e33d3101a9d11d46a4cb2658437246
Sujeet <sujeetgupta16@gmail.com>
2023-10-18 09:31:03 +0530

    java-library plugin with api dependency type for compile time dependency to next dependent project

--- HibernateRepository/src/main/java/org/example/Main.java
+++ HibernateRepository/src/main/java/org/example/Main.java
@@ -1,11 +1,11 @@
+package org.example;
+
 import org.apache.commons.lang3.StringUtils;
 
 public class Main {
     public static void main(String[] args) {
-        if (StringUtils.isBlank("NotBlank")) {
+        if(StringUtils.isBlank("")){
             System.out.println("Blank");
-        } else {
-            System.out.println("NotBlank");
         }
         System.out.println("Hello world!");
     }
